11ccb3d4c96209b09ea1b7b523a0fa914a0d8b62
abhimran <abhimrann@gmail.com>
2022-01-11 09:34:06 +0600

    Added styled component

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  height: 60px;
+  background: #000000;
+  color: #166191;
+  width: 100%;
+`;
+
+const Navbar = () => {
+  return <Container>Hello from Navbar!</Container>;
+};
+
+export default Navbar;
