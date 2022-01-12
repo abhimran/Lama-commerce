7f3873df65f47f720406be51a40800d9ddca2a06
abhimran <abhimrann@gmail.com>
2022-01-12 09:38:09 +0600

    Working with navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,60 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Search } from '@material-ui/icons';
 
 const Container = styled.div`
   height: 60px;
-  background: #000000;
-  color: #166191;
-  width: 100%;
 `;
 
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+const Right = styled.div`
+  flex: 1;
+`;
+const Center = styled.div`
+  flex: 1;
+`;
+
+const Language = styled.span`
+  cursor: pointer;
+  font-size: 14px;
+`;
+
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input``;
+
 const Navbar = () => {
-  return <Container>Hello from Navbar!</Container>;
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input />
+            <Search />
+          </SearchContainer>
+        </Left>
+        <Center>Center</Center>
+        <Right>Right</Right>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default Navbar;
