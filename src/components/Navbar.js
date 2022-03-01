27d0eb26b2a6d722902e20132536ef222fa735fd
abhimran <abhimrann@gmail.com>
2022-03-01 10:58:40 +0600

    created navbar and announcement component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Search } from '@material-ui/icons';
+import { Search, ShoppingCartOutlined } from '@material-ui/icons';
+import { Badge } from '@material-ui/core';
 
 const Container = styled.div`
   height: 60px;
@@ -19,9 +20,17 @@ const Left = styled.div`
 `;
 const Right = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
 `;
 const Center = styled.div`
   flex: 1;
+  text-align: center;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
 `;
 
 const Language = styled.span`
@@ -37,7 +46,15 @@ const SearchContainer = styled.div`
   padding: 5px;
 `;
 
-const Input = styled.input``;
+const Input = styled.input`
+  border: none;
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+`;
 
 const Navbar = () => {
   return (
@@ -47,11 +64,21 @@ const Navbar = () => {
           <Language>EN</Language>
           <SearchContainer>
             <Input />
-            <Search />
+            <Search style={{ color: 'gray', fontSize: '16px' }} />
           </SearchContainer>
         </Left>
-        <Center>Center</Center>
-        <Right>Right</Right>
+        <Center>
+          <Logo>LAMA.</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color='primary'>
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
+        </Right>
       </Wrapper>
     </Container>
   );
