a2341354f68a99a06c722d28704cb5fd9496a6b7
abhimran <abhimrann@gmail.com>
2022-03-01 23:51:33 +0600

    Created custom slider

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: '/images/slider-1.png',
+    title: 'WINTER SALE',
+    desc: "DON'T COMPROMIZE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: 'EFDDD3',
+  },
+  {
+    id: 2,
+    img: '/images/slider-1.png',
+    title: 'AUTUMN SALE',
+    desc: "DON'T COMPROMIZE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: 'EFDDD3',
+  },
+  {
+    id: 3,
+    img: '/images/slider-1.png',
+    title: 'POPULAR SALE',
+    desc: "DON'T COMPROMIZE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: 'EFDDD3',
+  },
+];
