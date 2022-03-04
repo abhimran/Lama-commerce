0575b0befd77553275e97d98c89c6690fe14fffe
abhimran <abhimrann@gmail.com>
2022-03-04 12:44:23 +0600

    Created category and products

--- src/data.js
+++ src/data.js
@@ -4,20 +4,73 @@ export const sliderItems = [
     img: '/images/slider-1.png',
     title: 'WINTER SALE',
     desc: "DON'T COMPROMIZE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-    bg: 'EFDDD3',
+    bg: 'f5fafd',
   },
   {
     id: 2,
     img: '/images/slider-1.png',
     title: 'AUTUMN SALE',
     desc: "DON'T COMPROMIZE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-    bg: 'EFDDD3',
+    bg: 'fcf1ed',
   },
   {
     id: 3,
     img: '/images/slider-1.png',
     title: 'POPULAR SALE',
     desc: "DON'T COMPROMIZE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-    bg: 'EFDDD3',
+    bg: 'fbf0f4',
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: '/images/shirt.webp',
+    title: 'SHIRT STYLE',
+  },
+  {
+    id: 2,
+    img: '/images/loungware.jpg',
+    title: 'LOUNGEWEAR LOVE',
+  },
+  {
+    id: 3,
+    img: '/images/jacket-1.jpg',
+    title: 'LIGHT JACKETS',
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: '/images/product-1.png',
+  },
+  {
+    id: 2,
+    img: '/images/product-2.png',
+  },
+  {
+    id: 3,
+    img: '/images/product-3.png',
+  },
+  {
+    id: 4,
+    img: '/images/product-4.png',
+  },
+  {
+    id: 5,
+    img: '/images/product-5.png',
+  },
+  {
+    id: 6,
+    img: '/images/product-6.png',
+  },
+  {
+    id: 7,
+    img: '/images/product-7.png',
+  },
+  {
+    id: 8,
+    img: '/images/product-9.png',
   },
 ];
